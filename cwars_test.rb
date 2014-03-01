3c06a828e3025bb5654f8cf22762d6e5d3a0bad0
sgpl <sharad45@gmail.com>
2014-03-01 18:24:40 +0300

    testing out my codewars soln

---
+++ cwars_test.rb
@@ -0,0 +1,33 @@
+def solution(str)
+   p str.reverse
+end
+
+solution('world')
+
+
+
+
+
+# def solution(nums)
+# 	if nums.empty? or nums.nil?
+# 		puts Array.new
+# 	else
+# 		puts nums.sort()
+# 	end
+# end
+
+# # solution [2, 1, 5, 3, 8, 4]
+# solution []
+
+# arr1 = [1,2,3,4,5,6,7,8]
+
+# def no_odds( values )
+#   # Return only non-odd values
+#   for i in values
+#   	if (values)%2 == 0
+#   		puts values
+#   	end
+#   end
+# end
+
+# no_odds arr1
