09110f5b41337142ccb86ff55e61fd9ab0531e88
sgpl <sharad45@gmail.com>
2014-03-07 13:02:02 +0300

    commented out old soln. for sentence from parts. fails a lot of edge cases.

--- sentence_frm_parts.rb
+++ sentence_frm_parts.rb
@@ -1,22 +1,8 @@
 def make_sentence parts
-	new_ar = parts.join(" ").split(".")
-	
-	ar_2 = new_ar.join("").split(" ")
-	ar_3 = []
-	ar_3 << ar_2.first
-	for i in ar_2[1..-1]
-		if i != ","
-			ar_3 << (" " + i)
-		else
-			ar_3 << i
-		end
-	end
-	ar_3 << "."
-	p ar_3.join("")
 end
 
-make_sentence ["hello", "what", ",","mango", ".", "."]
 
+make_sentence ["hello", "what", ",","mango", ".", "."]
 make_sentence ['hello', ',', 'my', 'dear'] # returns 'hello, my dear.'
 
 
@@ -29,3 +15,20 @@ make_sentence ['hello', ',', 'my', 'dear'] # returns 'hello, my dear.'
 # - there must always be a space between words;
 # - there must not be a space between a comma and word on the left;
 # - there must always be one and only one period at the end of a sentence.
+
+# def make_sentence parts
+# 	new_ar = parts.join(" ").split(".")
+	
+# 	ar_2 = new_ar.join("").split(" ")
+# 	ar_3 = []
+# 	ar_3 << ar_2.first
+# 	for i in ar_2[1..-1]
+# 		if i != ","
+# 			ar_3 << (" " + i)
+# 		else
+# 			ar_3 << i
+# 		end
+# 	end
+# 	ar_3 << "."
+# 	p ar_3.join("")
+# end
